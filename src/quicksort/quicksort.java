a3a1d347cb455dd442b5301a58271b2f39d39aa0
k1sheng <patsk9@gmail.com>
2015-10-31 10:32:01 -0700

    quick sort done, with basic unit tests

--- src/quicksort/quicksort.java
+++ src/quicksort/quicksort.java
@@ -1,7 +1,37 @@
 package quicksort;
 
 public class quicksort {
-	public static void sort(int[] origin){
+	
+	public static void sort(int[] nums){
+		if(nums==null || nums.length<2) return;
+		sort(nums, 0, nums.length-1);
+	}
+	
+	public static void sort(int[] nums, int start, int end){
+		int i = start;
+		int j = end;
+		int pivot = nums[(i+j)/2];
 		
+		while(i<=j){
+			while(nums[i]<pivot) i++;
+			
+			while(nums[j]>pivot) j--;
+			
+			if(i<=j){
+				swap(nums, i, j);
+				i++;
+				j--;
+			}
+		}
+		
+		if(i<end) sort(nums, i, end);
+		
+		if(start<j) sort(nums, start, j);
+	}
+	
+	private static void swap(int[] nums, int index1, int index2){
+		int tmp = nums[index1];
+		nums[index1] = nums[index2];
+		nums[index2] = tmp;
 	}
 }
